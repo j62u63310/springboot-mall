8edaacf43161c20f7be67e5c5f2808147bdf0e57
sa8a5 <sa8a56@gmail.com>
2024-01-12 10:57:38 +0800

    初始化 Spring boot

---
+++ src/test/java/com/j62u63310/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j62u63310.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
