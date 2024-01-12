c0641f761b901c66ca9fa7aff0285b14c7821078
sa8a5 <sa8a56@gmail.com>
2024-01-12 15:43:26 +0800

    商品功能 - 修改商品

--- src/main/java/com/j62u63310/springbootmall/service/ProductService.java
+++ src/main/java/com/j62u63310/springbootmall/service/ProductService.java
@@ -7,4 +7,6 @@ import com.j62u63310.springbootmall.model.Product;
 public interface ProductService {
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId,ProductRequest productRequest);
 }
